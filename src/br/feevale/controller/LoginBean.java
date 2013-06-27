1dd51a3c80f32e8dc2253ce1fead0c1335588cb3
gabrielhof <gabrielhof@gmail.com>
2013-06-27 16:57:54 -0300

    Initial Commit

---
+++ src/br/feevale/controller/LoginBean.java
@@ -0,0 +1,17 @@
+package br.feevale.controller;
+
+import br.feevale.model.bean.Usuario;
+
+public class LoginBean {
+
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+}
